677019c6c940e0b964575074d9f7d403273d7a98
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-08-04 16:58:56 +0200

    added heroku to package

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+const Twit = require('twit');
 
 app.use('/static', express.static('css'));
 
@@ -8,6 +9,14 @@ app.set('view engine', 'pug');
 app.get('/', (req, res) => {
     res.render('index');
 
+});
+
+const T = new Twit({
+    consumer_key:         '...',
+    consumer_secret:      '...',
+    access_token:         '...',
+    access_token_secret:  '...',
+    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 });
 
 app.get('/hello', (req, res) => {
