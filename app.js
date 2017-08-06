758f03f089da5b7e2a2b1990942d1ed2c1bf277a
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-08-06 17:47:07 +0200

    twitter username image added

--- app.js
+++ app.js
@@ -1,26 +1,89 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const Twit = require('twit');
+const config = require("./config");
+
 
 const app = express();
-const Twit = require('twit');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
 
 app.use('/static', express.static('css'));
+app.use('/static-images', express.static('images'));
 
 app.set('view engine', 'pug');
-app.get('/', (req, res) => {
-    res.render('index');
 
-});
 
-const T = new Twit({
-    consumer_key:         '...',
-    consumer_secret:      '...',
-    access_token:         '...',
-    access_token_secret:  '...',
-    timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-});
+const T = new Twit(config);
+
+let userName = "";
+let name = "";
+let profileImage = "";
+let tweet = "";
+let backgroundImgUrl = "";
+let myTweets;
+let myFriends;
+let myChats;
+let friendsCount = "";
+
+    T.get('account/verify_credentials', { skip_status: true })
+        .catch(function (err) {
+            console.log('caught error', err.stack)
+        })
+        .then(function (result) {
+            let twitter = result.data;
+            userName = twitter.screen_name;
+            profileImage = twitter.profile_image_url;
+            name = twitter.name;
+            tweet = twitter.tweet;
+            friendsCount = twitter.friends_count;
+            console.log(twitter.profile_image_url)
+
+            //console.log('data', result.data);
+            T.get("friends/list",
+                {screen_name: userName, count: 5},
+                function (err, data, response) {
+                if (err) {
+                    console.log(err);
+                    throw err;
+                } else {
+                    myFriends = data.users;
+                    //console.log(data.users);
+                }
+            });
+        });
+
+
+
 
 app.get('/hello', (req, res) => {
-    res.render('card', {prompt:"who is me", hint: "think"});
+    res.render('card', {prompt: 'hello'});
+});
+
+app.post('/', (req, res) => {
+    //console.dir(req.body)
+    res.render('card', {prompt: 'hello'});
+});
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        userName: userName,
+        profileImage: profileImage,
+        name: name,
+        tweet: tweet,
+        friendsCount: friendsCount
+
+
+    });
+});
+
+
+
+
+app.post('/', (req, res) => {
+    res.json(req.body)
 
 });
 
---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key: 'mrIBgaIWJAe0UZ97hbuPePI3l'
+    , consumer_secret: '23lupesQi2GuZfVDmvEA10jsoajFjCUGXw6XiuQGquzj7pHFos'
+    , access_token: '271871022-aapyp53vVTQtyi3upCqmJPEmerznPSmXyrr4RLvW'
+    , access_token_secret: 'LpW3F5joFMqJEZPZTAlE44hCGpZTkA1oEXAieFV4FGOaP'
+};
