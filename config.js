ef517c72b17c2c9ac39cc9665689900400dc820c
Debbie O'Brien <debs-obrien@users.noreply.github.com>
2017-08-08 15:51:21 +0200

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    consumer_key: 'jqAtQHuCYN5RX7xux8fbMCExG'
-    , consumer_secret: '9iKgDz9xgtH5kypCIkLILPdoeWFnlHE5jQBGSuYWRCLgKrTZKb'
-    , access_token: '271871022-lxH0nwJQrZvCkkCP7xpTWCdSqs2Zanj1Yv3xLybd'
-    , access_token_secret: 'vMfh8OOi6dm8p5qymBGx2SMtPcMaTrO3HFdAwgmN0Ur46'
-};
